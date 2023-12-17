81d970185cacc528d8e5453c38b3b39eec75e409
mumiak-mitch <mumiaktech@gmail.com>
2023-12-17 13:03:57 +0300

    User interface added

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
-// Step 2: Get computer's choice
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
+// script.js
 
-// Step 3: Play a single round
-function playRound(playerSelection, computerSelection) {
-    // Make playerSelection case-insensitive
-    playerSelection = playerSelection.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
-    // Check for ties and re-play the round
-    if (playerSelection === computerSelection.toLowerCase()) {
-        return "It's a tie! Play again.";
-    }
+document.getElementById("rock").addEventListener("click", function () {
+    playRound("rock");
+});
 
-    // Determine the winner of the round
-    if (
+document.getElementById("paper").addEventListener("click", function () {
+    playRound("paper");
+});
+
+document.getElementById("scissors").addEventListener("click", function () {
+    playRound("scissors");
+});
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        displayResult("It's a tie! Play again.");
+    } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
+        displayResult(`You win! ${playerSelection} beats ${computerSelection}.`);
+        playerScore++;
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+        displayResult(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        computerScore++;
+    }
+
+    updateScore();
+    checkWinner();
+}
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function displayResult(message) {
+    document.getElementById("result").textContent = message;
+}
+
+function updateScore() {
+    document.getElementById("score").textContent = `Score: Player ${playerScore} - ${computerScore} Computer`;
+}
+
+function checkWinner() {
+    if (playerScore === 5) {
+        displayResult("Congratulations! You win the game.");
+        resetGame();
+    } else if (computerScore === 5) {
+        displayResult("Sorry, you lose the game.");
+        resetGame();
     }
 }
 
-// Step 4: Play a best-of-five game
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
-        const computerSelection = getComputerChoice();
-
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-        // Update scores based on the result
-        if (result.includes('win')) {
-            playerScore++;
-        } else if (result.includes('lose')) {
-            computerScore++;
-        }
-    }
-
-    // Determine the overall winner
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win the game.");
-    } else if (playerScore < computerScore) {
-        console.log("Sorry, you lose the game.");
-    } else {
-        console.log("It's a tie in the overall game.");
-    }
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
 }
-
-// Start the game
-game();
